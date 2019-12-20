75e504d27c8644855cf5456a20ac73ae4f3a3495
Fernando Ferreira <oakley-hbs@hotmail.com>
2019-12-20 16:17:07 -0300

    v2.01

--- Outliner/app.py
+++ Outliner/app.py
@@ -5,6 +5,7 @@ from pprint import pprint
 def main ():
     reader = PageReader()
     reader.url = 'https://oglobo.globo.com/esportes/zico-exclusivo-quero-ver-torcida-feliz-nao-estou-preocupado-se-vao-lembrar-de-mim-24148095'
+    # reader.url = 'https://www1.folha.uol.com.br/poder/2019/12/ex-governador-ricardo-coutinho-e-preso-sob-suspeita-de-desvio-de-verba-na-paraiba.shtml'
 
     dump_json = reader.dump_json
 
--- Outliner/core/engine.py
+++ Outliner/core/engine.py
@@ -1,11 +1,15 @@
+# utf-8
+
 from bs4 import BeautifulSoup
 import requests
+import codecs
 
 class PageReader (object):
     def __init__ (self):
         self._json = {}
         self._html = ''''''
 
+        self.file = codecs.open("debug.html","w","utf-8")
 
     @property
     def url (self):
@@ -19,35 +23,67 @@ class PageReader (object):
     def dump_json (self):
         page = self.scrap_soup
 
-        header = page.find('header')
+        header = page.find('head')
+        article = page.find('article')
 
-        title = page.find("meta",  property="og:title")['content']
-        pre_title = page.find("meta",  property="og:description")['content']
+        title       = page.find("meta", property="og:title")['content']
+        pre_title   = page.find("meta", property="og:description")['content']
+        post_image  = page.find("meta", property="og:image")['content']
         site_origin = page.find("meta", property="og:site_name")['content']
+        _keys       = page.find_all("meta",property="article:tag")
 
-        images = header.find_all('img')
+        keywords = []
 
-        print(images)
+        for k in _keys:
+            key_content = str(k['content']).split(',')
+            keywords = keywords + key_content
+
+        raw_paragraphs = article.find_all("p")
+        paragraphs_config = []
 
 
-        body = page.find('main')
-        paragraphs = body.find_all('p')
-        text_p = [p.text for p in paragraphs]
-        
+        for p in raw_paragraphs:
+            p_json = {}
+            
+            if not p.find("strong") == None:
+                pass
+
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+                # VOCÊ PAROU AQUI
+
+
+        self.file.write(str(paragraphs))
 
         self._json = {
-            'title':title,
-            'pre_title':pre_title,
-            'body':'text_p'
+            'title'       : title,
+            'pre_title'   : pre_title,
+            'site_origin' : site_origin,
+            'post_image'  : post_image,
+            'keywords'    : keywords,
+            'body'        :'paragraphs'
         }
         return self._json
 
     @property
     def scrap_soup (self):
         page = requests.get(self.url)
-        soup = BeautifulSoup(page.text, 'html.parser')
+        soup = BeautifulSoup(page.content, 'html.parser')
         return soup
 
     @property
     def dump_html (self):
-        return 'article'
+        return '<p>sex</p>'
